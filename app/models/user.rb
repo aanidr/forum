1ef4524d650db1552a6b4d3f7c87a4f8cac973f3
Adrian Nawrocki <adrian.nawrocki@netguru.co>
2019-02-01 02:46:40 +0100

    Form object finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 6}, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, length:{minimum: 6}
-
+  
   has_secure_password
 
   has_many :topics
